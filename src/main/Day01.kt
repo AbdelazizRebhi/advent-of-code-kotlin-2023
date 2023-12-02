72055e63c82e59c1caf0d44e26455e54983dbeb9
AbdelazizRebhi <rebhi.abdelaziz@gmx.de>
2023-12-02 17:26:03 +0100

    chore(Day01): Solve day 1 part 1

--- src/main/Day01.kt
+++ src/main/Day01.kt
@@ -2,17 +2,29 @@ package main
 
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var sum = 0
+        val regex = """\d""".toRegex()
+        input.forEach {
+            val digits = regex
+                .findAll(it)
+                .map(MatchResult::value)
+                .toList()
+
+            sum += (digits.first() + digits.last()).toInt()
+        }
+        return sum
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val input = readInput(
+        "Day01"
+//        "Day01_test"
+    )
 
-    val input = readInput("Day01")
     part1(input).println()
-    part2(input).println()
+
+    //part2(input).println()
 }
