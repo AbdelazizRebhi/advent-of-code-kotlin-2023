60e6580c39df5089045ecd0bd2a87696795012e0
AbdelazizRebhi <rebhi.abdelaziz@gmx.de>
2023-12-03 11:09:29 +0100

    chore(Day02): Solve day 2 part 1

---
+++ src/main/Day02.kt
@@ -0,0 +1,54 @@
+package main
+
+fun main() {
+    fun part1(input: List<String>): Int = input
+        .map(String::toGame)
+        .filter(Game::isPossible)
+        .sumOf(Game::id)
+
+    fun part2(input: List<String>): Int = input.sumOf { it.length }
+
+    val input = readInput(
+        "Day02"
+//        "Day02_test"
+    )
+
+    part1(input).println()
+
+    part2(input).println()
+}
+
+enum class Color { RED, GREEN, BLUE }
+
+data class Cube(val color: Color, val number: Int)
+fun String.toCube() = Cube (
+    Color.valueOf(this.substringAfter(" ").uppercase()),
+    this.substringBefore(" ").toInt()
+)
+data class Turn(val cubes: List<Cube>) {
+    operator fun get(color: Color): Int {
+        return cubes.associateBy { it.color }[color]?.number ?: 0
+    }
+}
+
+fun String.toTurn() = Turn (
+    split(", ").map(String::toCube)
+)
+
+data class Game(val id: Int, val turns: List<Turn>) {
+    operator fun get(color: Color) = turns.maxByOrNull { it[color] }!![color]
+
+    fun isPossible(): Boolean {
+        return (this[Color.RED] < 13) && (this[Color.GREEN] < 14) && (this[Color.BLUE] < 15)
+    }
+}
+
+fun String.toGame() = Game (
+    substringBefore(": ").substringAfter(" ").toInt(),
+    substringAfter(": ").split("; ").map(String::toTurn)
+)
+
+
+
+
+
