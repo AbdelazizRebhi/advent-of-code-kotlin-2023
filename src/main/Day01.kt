06d65b2572747bacc00cf9d67955c4d18361ba9a
AbdelazizRebhi <rebhi.abdelaziz@gmx.de>
2023-12-03 09:07:53 +0100

    refactor(Day01): It is still a mess but who cares

--- src/main/Day01.kt
+++ src/main/Day01.kt
@@ -1,39 +1,13 @@
 package main
 
 fun main() {
-    fun part1(input: List<String>): Int {
-        var sum1 = 0
-
-        input.forEach {
-            val digits = "\\d".toRegex().findAll(it).map(MatchResult::value).toList()
-
-            sum1 += (digits.first() + digits.last()).toInt()
-        }
-
-        return sum1
+    fun part1(input: List<String>): Int = input.sumOf {
+        line -> findNumericDigitsInLine(line).toCalibrationValue()
     }
 
-    fun part2(input: List<String>): Int {
-        var sum2 = 0
-
-        val digitRegex = buildList {
-            add("\\d")
-            addAll(Digit.entries.map { it.name.lowercase() })
-        }.map(String::toRegex)
-
-        input.forEach { line ->
-            val lineMatches = digitRegex.flatMap { regex ->
-                regex.findAll(line).map(MatchResult::toPair)
-            }.toList().sortedBy(Pair<Int, Int>::first)
-
-            sum2 += lineMatches.first().second * 10 + lineMatches.last().second
-        }
-
-        return sum2
+    fun part2(input: List<String>): Int = input.sumOf {
+        line -> findValidDigitsInLine(line).toCalibrationValue()
     }
-
-
-
 
     val input = readInput(
         "Day01"
@@ -44,5 +18,41 @@ fun main() {
 
     part2(input).println()
 }
+
+// Valid digits = worded digits +  numeric digits
+
+fun findNumericDigitsInLine(line: String) = "\\d".toRegex()
+    .findAll(line)
+    .map(MatchResult::value)
+    .map(String::toInt)
+    .toList()
+
+fun List<Int>.toCalibrationValue() = this.first() * 10 + this.last()
+
+enum class WordedDigit {
+    ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE,
+}
+
+fun String.validDigitToInt(): Int = if (length > 1) {
+    WordedDigit.valueOf(this.uppercase()).ordinal + 1
+} else {
+    toInt()
+}
+
+fun MatchResult.toPair(): Pair<Int, Int> = this.range.first to this.value.validDigitToInt()
+
+val validDigitRegex = buildList {
+    add("\\d")
+    addAll(WordedDigit.entries.map { it.name.lowercase() })
+}.map(String::toRegex)
+
+// all occurrences of valid digits in a single line
+// as list of pairs: (index, digit), sorted by the index
+fun findValidDigitsInLine(line: String) = validDigitRegex.flatMap { regex ->
+    regex.findAll(line).map(MatchResult::toPair)
+}.toList().sortedBy(Pair<Int, Int>::first)
+
+fun List<Pair<Int, Int>>.toCalibrationValue() = this.first().second * 10 + this.last().second
+
 
 
