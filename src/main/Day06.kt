3eab8fa09c9a2ab9acaedca42c674cb98f2ecab5
AbdelazizRebhi <rebhi.abdelaziz@gmx.de>
2023-12-06 21:23:07 +0100

    chore(Day06): Solve day 6

---
+++ src/main/Day06.kt
@@ -0,0 +1,52 @@
+package main
+
+import kotlin.math.ceil
+import kotlin.math.floor
+import kotlin.math.sqrt
+
+fun main() {
+    val input = readInput(
+        "Day06"
+//        "Day06_test"
+    )
+
+    // part 1: multiple races
+    val times = input[0].toRaceNumberList()
+    val recordDistances = input[1].toRaceNumberList()
+    val races = times.zip(recordDistances).map { it.toRace() }
+
+    val part1 = races.map { it.getWinCount() }.reduce(Int::times)
+
+    part1.println()
+
+    // part 2: one race
+    val race = Race(input[0].toSingleRaceNumber(), input[1].toSingleRaceNumber())
+
+    val part2 = race.getWinCount()
+
+    part2.println()
+
+
+}
+
+fun Race.getWinCount(): Int {
+    val sqrtDelta = sqrt((time * time - 4.0 * recordDistance))
+    val x1 = floor(0.5 * (time - sqrtDelta) + 1).toInt()
+    val x2 = ceil(0.5 * (time + sqrtDelta) - 1).toInt()
+
+    return x2 - x1 + 1
+}
+
+data class Race(val time: Long, val recordDistance: Long)
+
+fun Pair<Long, Long>.toRace() = Race(first, second)
+
+fun String.toRaceNumberList(): List<Long> = substringAfter(":")
+    .trim()
+    .split(Regex("\\s+"))
+    .map { it.toLong() }
+
+fun String.toSingleRaceNumber(): Long = substringAfter(":")
+    .trim()
+    .replace(Regex("\\s+"), "")
+    .toLong()
