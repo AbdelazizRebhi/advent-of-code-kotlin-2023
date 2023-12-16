15c17811366e4f5f6b7642eebac4a4facceab87a
AbdelazizRebhi <rebhi.abdelaziz@gmx.de>
2023-12-16 14:07:26 +0100

    fix(Day08): Restore day 8

--- src/main/Day08.kt
+++ src/main/Day08.kt
@@ -1,24 +1,59 @@
 package main
 
-fun main() {
-
+suspend fun main() {
     val input = readInput(
-//        "Day08"
-        "Day08_test"
+        "Day08"
+//        "Day08_test"
     )
 
+    val instructions = input[0].map { Instruction(it) }
+    val network = input.drop(2).toNetwork()
 
-
-
-    val part1 = input.size
-
+    val part1 = network.countStepsToZZZ("AAA", instructions)
     part1.println()
 
-    val part2 = input.size
+    val part2 = network.ghosts.parallelMap { ghost ->
+        network.countStepsToGhostZ(ghost, instructions)
+    }.fold(1L) { acc, l -> lcm(acc, l) }
 
     part2.println()
-
-
 }
 
-data class Instruction(val letter: Char, val operation: (Pair<String, String>) -> String)
+fun Network.countStepsToZZZ(start: String, instructions: List<Instruction>): Long =
+    countStepsUntil(start, instructions) { it != "ZZZ"}
+
+fun Network.countStepsToGhostZ(ghost: String, instructions: List<Instruction>): Long =
+    countStepsUntil(ghost, instructions) { !it.endsWith("Z") }
+
+inline fun Network.countStepsUntil(
+    start: String,
+    instructions: List<Instruction>,
+    crossinline condition: (String) -> Boolean
+): Long {
+    var current = start
+    var stepCount = 0L
+    while (condition(current)) {
+        val index = (stepCount++ % instructions.size).toInt()
+        current = navigateFrom(current, instructions[index])
+    }
+    return stepCount
+}
+
+data class Network(val nodes: Map<String, Pair<String, String>>) {
+    val ghosts: List<String>
+        get() = nodes.keys.filter { it.endsWith("A") }
+}
+
+suspend fun List<String>.toNetwork() = Network(
+    parallelMap { it.split(Regex("\\W+")) }
+        .associate { it[0] to (it[1] to it[2]) }
+)
+
+data class Instruction(val letter: Char)
+
+fun Network.navigateFrom(start: String, instruction: Instruction): String =
+        if (instruction.letter == 'L') {
+            nodes[start]!!.first
+        } else {
+            nodes[start]!!.second
+        }
